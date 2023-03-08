59860515ae108910435e6e4f8de12094f47eca59
ghkdgtjd <68437864+ghkdgtjd@users.noreply.github.com>
2023-03-08 21:36:05 +0900

    stop and see the oni

--- 01_2_p01.cpp
+++ 01_2_p01.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 
+void swap(int a, int b)
+{
+    num1=
+}
+
+void swap(char c)
+{
+    ????
+}
+
+void swap(int a, int b)
+{
+    ???
+}
+
+int main(void)
+{
+    int num1=20, num2=30;
+    swap(&num1, &num2);
+    std::cout<<num1<<' '<num2<<std::endl;
+
+    char ch1='A', ch2='Z'
+    swap(&ch1, &ch2);
+    std::cout<<ch1<<' '<ch2<<std::endl;
+
+    double dbl1=1.111, dbl2=5.555;
+    swap(&dbl1, &dbl2);
+    std::cout<<dbl1<<' '<dbl2<<std::endl;
+    return 0;
+}
