67ab8bd3d3e0060b2828b4c9131e2bbb080dd5f5
ghkdgtjd <68437864+ghkdgtjd@users.noreply.github.com>
2023-03-09 17:23:52 +0900

    good job today

--- 00_0_00.cpp
+++ 00_0_00.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
-/** 매일매일 정해진 공부를 마치고 게임을 할 수 있도록 하자 현성아 내일도 화이팅...!
+
+void swap(int* a, int* b)
+{
+    std::cout<<b<<"\n"<<*b<<"\n"<<a<<"\n"<<*a<<std::endl;
+    static int num1 = *b;
+    std::cout<<b<<"\n"<<*b<<"\n"<<a<<"\n"<<*a<<std::endl;
+    static int num2 = *a;
+    std::cout<<b<<"\n"<<*b<<"\n"<<a<<"\n"<<*a<<std::endl;
+    std::cout<<num1<<' '<<num2<<std::endl;
+
+}
+
+int main(void)
+{
+    int num1 = 10, num2=20;
+    swap(&num1, &num2);
+    std::cout<<num1<<' '<<num2<<std::endl;
+
+    return 0;
+}
--- 01_2_p01.cpp
+++ 01_2_p01.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 
-void swap(int a, int b)
+void swap(int* a, int* b)
 {
-    num1=
+    int num1 = *b;
+    int num2 = *a;
 }
 
-void swap(char c)
+void swap(char* a, char* b)
 {
-    ????
+    char ch1 = *b;
+    char ch2 = *a;
 }
 
-void swap(int a, int b)
+void swap(double* a, double* b)
 {
-    ???
+    double dbl1 = *b;
+    double dbl2 = *a;
 }
+
 
 int main(void)
 {
     int num1=20, num2=30;
     swap(&num1, &num2);
-    std::cout<<num1<<' '<num2<<std::endl;
+    std::cout<<num1<<' '<<num2<<std::endl;
 
-    char ch1='A', ch2='Z'
+    char ch1='A', ch2='Z';
     swap(&ch1, &ch2);
-    std::cout<<ch1<<' '<ch2<<std::endl;
+    std::cout<<ch1<<' '<<ch2<<std::endl;
 
     double dbl1=1.111, dbl2=5.555;
     swap(&dbl1, &dbl2);
-    std::cout<<dbl1<<' '<dbl2<<std::endl;
+    std::cout<<dbl1<<' '<<dbl2<<std::endl;
     return 0;
 }
