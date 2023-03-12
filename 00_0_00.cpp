5d95c98ee9ca0e3e4ab8f72af95ac98eea4cb9d3
ghkdgtjd <68437864+ghkdgtjd@users.noreply.github.com>
2023-03-12 23:04:20 +0900

    weekend end

--- 00_0_00.cpp
+++ 00_0_00.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 
-void swap(int* a, int* b)/** a = num1, b=num2*/
-{
-    int c = *a;
-    std::cout<<a<<' '<<*a<<' '<<b<<' '<<*b<<' '<<c<<std::endl;
-    *a = *b;
-    std::cout<<a<<' '<<*a<<' '<<b<<' '<<*b<<' '<<c<<std::endl;
-    *b = c;
-    std::cout<<a<<' '<<*a<<' '<<b<<' '<<*b<<' '<<c<<std::endl;
-}
-
 int main(void)
 {
-    int num1 = 10, num2=20;
-    swap(&num1, &num2);
-    std::cout<<&num1<<' '<<&num2<<std::endl;
-    std::cout<<num1<<' '<<num2<<std::endl;
-
     return 0;
 }
